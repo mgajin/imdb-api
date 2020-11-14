be07af8467910b5f2def888642d6af45d19a8546
Marko <markogajiin@gmail.com>
2020-11-14 13:23:03 +0100

    App config.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+require('colors')
 const app = require('./app')
 
-app.listen(3001, console.log('Server running...'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}...`.yellow.bold)
+})
