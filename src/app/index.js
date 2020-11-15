40045fbb827d7abaf0adade7f1cc730400aaf4bb
Marko <markogajiin@gmail.com>
2020-11-15 18:37:24 +0100

    CORS enabled.

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,13 @@
 require('module-alias/register')
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 app.use('/api/v1', require('@api/routes'))
 
